3f42b9b3fde9bea5f0e7c2d7dc77d9bf377da7ac
sangeeth <sangeethmgd5@gmail.com>
2025-06-21 14:01:12 +0530

    path updated

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -11,24 +11,32 @@ export const projectsData = [
         image: Crypto,
         title: "Cryptodigger",
         category: "web",
+        url:"https://cryptodigger.netlify.app/",
+        githuburl:"https://github.com/sangeeth-mk/CryptoDigger"
     },
     {
         id: 2,
         image: Field,
         title: "Field Management System",
         category: "app",
+        url:"http://64.227.153.239:4060/login",
+        githuburl:"https://bitbucket.org/fieldesk/fieldeskgo/src/master/src/"
     },
     {
         id: 3,
         image: Netflix,
         title: "Netflix Clone",
         category: "design",
+        url:"",
+        githuburl:"https://github.com/sangeeth-mk/netflix-react",
     },
     {
         id: 4,
         image: Watch,
         title: "Watch Landing Page",
         category: "app",
+        url:"",
+        githuburl:""
     },
 ];
 
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,8 +5,11 @@ const WorkItems = ({ item }) => {
         <div className="work__card" key={item.id}>
             <img src={item.image} alt="" className='work__img' />
             <h3 className="work__title">{item.title}</h3>
-            <a href="#portfolio" className="work__button">
+           <a href={item.url} className="work__button">
                 Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            </a>
+            <a href={item.githuburl} className="work__button">
+                Github <i className="bx bxl-github work__button-icon"></i>
             </a>
         </div>
     )
